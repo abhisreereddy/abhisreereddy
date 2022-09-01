e9f2d3aa51aaa46b5de4d79af3bcd80b7b008830
abhisreereddy <112600736+abhisreereddy@users.noreply.github.com>
2022-09-01 08:59:37 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,6 @@
+rows=int(input("enter tne no of rows:"))
+column=int(input("enter the no of column:"))
+for i in range (rows):
+     for j in range (column):
+          print("*",end='')
+     print()
