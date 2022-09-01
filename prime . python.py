377ef19e387c8ed3c442179f1c1a0093ef890299
abhisreereddy <112600736+abhisreereddy@users.noreply.github.com>
2022-09-01 08:56:18 +0530

    Add files via upload

---
+++ prime . python.py
@@ -0,0 +1,21 @@
+n = int(input('Enter : '))
+prime_numbers = [2,3]
+i=3
+
+if(0<n<3):
+    print(n,'th Prime Number is :',primenumbers[n-1])
+elif(n>2):
+    while (True):
+        i+=1
+        status = True
+        for j in range(2,int(i/2)+1):
+            if(i%j==0):
+                status = False
+                break
+        if(status==True):
+            prime_numbers.append(i)
+        if(len(prime_numbers)==n):
+            break
+    print(n,'th Prime Number is :', primenumbers[n-1])
+else:
+    print('Please Enter A Valid Number')
