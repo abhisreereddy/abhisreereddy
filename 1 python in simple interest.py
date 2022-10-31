516ab83e74a02868d0ed1c3df4070bd9837f05b6
abhisree <112600736+abhisreereddy@users.noreply.github.com>
2022-10-31 11:18:24 +0530

    Update 1 python in simple interest.py

--- 1 python in simple interest.py
+++ 1 python in simple interest.py
@@ -1,4 +1,4 @@
-#Python program to calculate simple interest
+
 
 P = float(input("Enter the principal amount : "))
 
@@ -6,8 +6,7 @@ N = float(input("Enter the number of years : "))
 
 R = float(input("Enter the rate of interest : "))
 
-#calculate simple interest by using this formula
+
 SI = (P * N * R)/100
 
-#print
 print("Simple interest : {}".format(SI))
